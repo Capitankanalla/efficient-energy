4dd202b7dfc20429416f6d4b9360c0a57868a0ab
capitan kanalla <nanoxtechnologies@gmail.com>
2026-04-30 10:12:15 +0200

    fix: why consultor. Fallo a la ruta del botó. Soluconat, contingut carrega adequadament

--- js/seccions.js
+++ js/seccions.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // 1) Carregar JSON d'idioma
-    fetch("lang/ca.json")
+    fetch("json/lang/ca.json")
         .then(r => r.json())
         .then(data => {
             carregarSeccioConsultor(data.whyConsultant);
