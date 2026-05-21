43de8fb77fe2c39e0e97c3d599c6a7682f0b3e2a
capitan kanalla <nanoxtechnologies@gmail.com>
2026-05-21 10:08:55 +0200

    feat: política de privacitat

--- form/js/form.js
+++ form/js/form.js
@@ -19,9 +19,32 @@ const popup = document.getElementById("privacyPopup");
 const acceptBtn = document.getElementById("acceptPrivacy");
 const rejectBtn = document.getElementById("rejectPrivacy");
 
+let legalData = null;
+
+fetch("../json/legals.json")
+  .then(res => res.json())
+  .then(data => legalData = data);
+
+
 // Desactivar checkbox i botó d'enviar
 legalCheck.disabled = true;
 submitBtn.disabled = true;
+
+// openPolicy.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   if (!legalData) return;
+
+//   const title = document.querySelector("#privacyPopup h2");
+//   const text = document.querySelector("#privacyPopup p");
+
+//   title.textContent = legalData.privacitat.titol;
+//   text.innerHTML = legalData.privacitat.contingut
+//     .map(line => `<p>${line}</p>`)
+//     .join("");
+
+//   popup.classList.remove("hidden");
+// });
 
 // Obrir popup
 openPolicy.addEventListener("click", (e) => {
